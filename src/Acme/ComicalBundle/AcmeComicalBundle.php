4329793c5ac6463fc955e84db3bc7642fa0462de
motohiro ishii(dj) <m.ece0113+git@gmail.com>
2012-07-11 05:36:19 +0900

    autoloadの設定の変更

---
+++ src/Acme/ComicalBundle/AcmeComicalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\ComicalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeComicalBundle extends Bundle
+{
+}
---
+++ src/Acme/ComicalBundle/Entity/MUser.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Acme\ComicalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Acme\ComicalBundle\Entity\MUser
+ *
+ * @ORM\Table(name="m_user")
+ * @ORM\Entity
+ */
+class MUser
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string $userId
+     *
+     * @ORM\Column(name="user_id", type="string", length=100, nullable=false)
+     */
+    private $userId;
+
+    /**
+     * @var string $password
+     *
+     * @ORM\Column(name="password", type="string", length=100, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @var string $mailAddress
+     *
+     * @ORM\Column(name="mail_address", type="string", length=100, nullable=false)
+     */
+    private $mailAddress;
+
+    /**
+     * @var boolean $socialFlg
+     *
+     * @ORM\Column(name="social_flg", type="boolean", nullable=false)
+     */
+    private $socialFlg;
+
+    /**
+     * @var datetime $createdAt
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    private $createdAt;
+
+    /**
+     * @var datetime $updatedAt
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    private $updatedAt;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param string $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return string 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set mailAddress
+     *
+     * @param string $mailAddress
+     */
+    public function setMailAddress($mailAddress)
+    {
+        $this->mailAddress = $mailAddress;
+    }
+
+    /**
+     * Get mailAddress
+     *
+     * @return string 
+     */
+    public function getMailAddress()
+    {
+        return $this->mailAddress;
+    }
+
+    /**
+     * Set socialFlg
+     *
+     * @param boolean $socialFlg
+     */
+    public function setSocialFlg($socialFlg)
+    {
+        $this->socialFlg = $socialFlg;
+    }
+
+    /**
+     * Get socialFlg
+     *
+     * @return boolean 
+     */
+    public function getSocialFlg()
+    {
+        return $this->socialFlg;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return datetime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
